f3e9328f6e02ad0acc9c8e6475e856141716280d
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-01-12 21:07:20 -0300

    add Box component

---
+++ packages/react/src/components/Box.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from 'react'
+
+import { styled } from '../styles'
+
+export const Box = styled('div', {
+  padding: '$4',
+  border: '1px solid $gray600',
+  borderRadius: '$md',
+  backgroundColor: '$gray800',
+})
+
+export type BoxProps = ComponentProps<typeof Box>
