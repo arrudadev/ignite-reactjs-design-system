3e417305e23537a8aab0afde9270101c117d1b13
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-01-13 22:06:24 -0300

    add Avatar component storie

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/arrudadev.png',
+    alt: 'Alexandre Arruda',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: '',
+  },
+}
